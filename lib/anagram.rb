8caf60f1aa73aed253ca2d9ed62f48036b57eae1
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-21 18:26:37 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+class Anagram
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match(array_words)
+    array_words.select do |element|
+      (@word.split("").sort) == (element.split("").sort)
+    end
+  end 
+
+
+end
+  
